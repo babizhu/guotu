aafb1ea7d9fbf6f37a7cea92ff72a2fba3d1e2ca
liukun <liu_kun@msn.com>
2017-02-25 23:52:44 +0800

    登陆命令完成

--- src/main/java/org/bbz/srxk/guotu/handler/cmd/LoginCmd.java
+++ src/main/java/org/bbz/srxk/guotu/handler/cmd/LoginCmd.java
@@ -2,7 +2,6 @@ package org.bbz.srxk.guotu.handler.cmd;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import org.bbz.srxk.guotu.handler.codec.IntProccessHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +13,11 @@ import java.util.Date;
  */
 public class LoginCmd extends AbstractCmd {
 
-    private static final Logger LOG = LoggerFactory.getLogger( LoginCmd.class );
+    private static final Logger LOG = LoggerFactory.getLogger(LoginCmd.class);
     private final ChannelHandlerContext ctx;
 
     private int clientId;
+
     public LoginCmd(ChannelHandlerContext ctx, ByteBuf data) {
         super(data);
         this.ctx = ctx;
@@ -29,7 +29,7 @@ public class LoginCmd extends AbstractCmd {
         ctx.writeAndFlush(buildServerTime());
     }
 
-    private ByteBuf buildServerTime(){
+    private ByteBuf buildServerTime() {
 //        F0 BB 78 52 0E 1E 23 0E 07 0B 07 23 E8
 //        F0 BB 78 52时分秒年月日星期23 E8   
 
@@ -45,10 +45,13 @@ public class LoginCmd extends AbstractCmd {
         timeBuf.writeByte(cal.get(Calendar.HOUR_OF_DAY));//时
         timeBuf.writeByte(cal.get(Calendar.MINUTE));//分
         timeBuf.writeByte(cal.get(Calendar.SECOND));//秒
-        timeBuf.writeByte(cal.get(Calendar.YEAR));//年
-        timeBuf.writeByte(cal.get(Calendar.MONTH));//月
+
+        timeBuf.writeByte(cal.get(Calendar.YEAR) % 2000);//年
+        timeBuf.writeByte(cal.get(Calendar.MONTH + 1));//月
         timeBuf.writeByte(cal.get(Calendar.DAY_OF_MONTH));//日
-        timeBuf.writeByte(cal.get(Calendar.DAY_OF_WEEK) -1);//星期
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        dayOfWeek = dayOfWeek == 1 ? 7 : dayOfWeek - 1;
+        timeBuf.writeByte(dayOfWeek);//星期
 
 
         timeBuf.writeByte(0x23);
@@ -57,10 +60,11 @@ public class LoginCmd extends AbstractCmd {
         return timeBuf;
     }
 
+
     @Override
     void parse() {
         clientId = data.getInt(2);
-        LOG.debug( "clientId is " + clientId);
+        LOG.debug("clientId is " + clientId);
 
     }
 }
